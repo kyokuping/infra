90fa1427de930fccc098c3aa923b749c24d17d11
kyokuping <me@kyoku.dev>
2026-09-09 21:20:26 +0900

    chore: configure R2 state backend

--- base.tf
+++ base.tf
@@ -1,5 +1,23 @@
 terraform {
-  required_version = "~> 1.10.3"
+  required_version = ">= 1.10.3"
+
+  backend "s3" {
+    bucket       = "mnr-state"
+    key          = "manura/terraform.tfstate"
+    region       = "auto"
+    use_lockfile = true
+
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
+
+    endpoints = {
+      s3 = "https://0dadaa1ae08a9c7fdbc54393c0a0e5c5.r2.cloudflarestorage.com"
+    }
+  }
 
   required_providers {
     tailscale = {
